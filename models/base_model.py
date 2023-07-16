20fa7fb656656014ff9021d5aedf353cd253da06
KwameNtaadu007 <kwamealabama@gmail.com>
2023-07-16 06:26:08 -0700

    refactored the code to meet pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -17,8 +17,8 @@ class BaseModel:
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
                         value = datetime.strptime(
-                            value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, value)
+                                value, "%Y-%m-%dT%H:%M:%S.%f")
+                        setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -37,4 +37,3 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-
